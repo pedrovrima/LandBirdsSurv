840ed72a5daec68a2802f1d35923d1c21839e908
pedrovrima <pedrovrima@gmail.com>
2017-02-08 15:25:44 -0800

    scratch

---
+++ caphist.R
@@ -0,0 +1,24 @@
+####Function to create different capture-history for 
+
+
+###Function###
+
+
+###########Load the Data################
+##Recap History##
+
+##Database##
+
+###########Cuts#########################
+#####Beggining#######
+##Do nothing##
+##1992 cut###
+##Specific cut##
+
+
+#####August####
+##Do nothing##
+##Remove the augustine individuals##
+
+
+#########
