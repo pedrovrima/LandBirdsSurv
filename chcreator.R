f777c64b432c1b36d2a2c8287513b609e0027f8d
pedrovrima <pedrovrima@gmail.com>
2017-02-09 11:55:47 -0800

    Remove individuals that had no capture after filters were add

--- chcreator.R
+++ chcreator.R
@@ -22,8 +22,9 @@
 ##2- "HY" - only birds first captured as HY
 ##3- "all" - all individuals on the raw CH
 
-filefold <- "/home/colibri/Documents/RSL/KBMN Capture Histories" #####Use the folder where the base capture files (csv) are - WITHOUT "/" in the end 
+filefold <- "/home/colibri/Documents/RSL/KBMN Capture Histories" #####The folder where the raw CH files (.csv) are - WITHOUT "/" in the end
 resulfold <- "/home/colibri/Documents/"##The folder where the final files will be stored - WITHOUT "/" in the end
+database <- "/home/colibri/Documents/RData/KBMNdata.RData"
 source("/home/colibri/Documents/RSL/Codes/caphist.R")#######Path to function code
 
 markch.creator(spp="SOSP",init.cut="1992")
