86a6045603c4543a0ec0dc0ea44da83d05b59780
pedrovrima <pedrovrima@gmail.com>
2017-02-08 17:25:28 -0800

    First Version - Able to do first cuts (none, 1992, first cap)

--- caphist.R
+++ caphist.R
@@ -1,24 +1,64 @@
 ####Function to create different capture-history for 
+setwd("/home/colibri/Documents/RSL")
 
-
+markch.creator <- function(spp,init.cut="none",age="AHY"){
 ###Function###
+
 
 
 ###########Load the Data################
 ##Recap History##
-
+tt <- read.csv(paste("KBMN Capture Histories/",spp,"_CapHistCovars.csv",sep=""),head=T)
 ##Database##
+
+###Select the important part of the table
+bnum <-tt[,2] ##BandNumbers
+chist <- which(substring(colnames(tt),1,3)=="CAP")
+
+
+
 
 ###########Cuts#########################
 #####Beggining#######
+
 ##Do nothing##
+if(init.cut=="none"){
+    caphist <- tt[,chist]}
+
 ##1992 cut###
+if(init.cut=="ninetwo"){
+    ninetwo <- chist[-which(as.numeric(substring(colnames(tt)[chist],5,9))<1992)]
+    caphist <- tt[,ninetwo]}
+
 ##Specific cut##
+if(init.cut=="firstcap"){
+    cols <- apply(tt[,chist],2,sum)
+    vv <- min(which(cols!=0))
+    firstcap <- chist[vv:length(chist)]
+    caphist <- tt[,firstcap]}
 
 
 #####August####
 ##Do nothing##
 ##Remove the augustine individuals##
+########
 
 
-#########
+###AGE####
+##Only AHY
+if(age=="AHY"){
+    ahygroup <- tt[,31]
+    rmvbird <- which(ahygroup==0)
+    caphist <- caphist[-rmvbird,]
+    bnum <- bnum[-rmvbird]
+    grp <- ahygroup[-rmvbird]
+}
+
+#####Create Table#####
+bands <- paste("/*",bnum,"*/")
+history <- apply(caphist,1,function(x)paste(x,collapse=""))
+info <- paste("/*",spp,"-",length(history),"Individuals -",ncol(caphist),"Ocasions */\r\n")
+
+
+cat(paste(c(info,paste(bands,history,paste(grp,";\r\n",sep=""))),collapse=""),file=paste(spp,init.cut,".inp",sep=""))
+}
